85500ef0018ddd9f9714d823d9c4244e7702d323
Tara Grimm <tararochegrimm@gmail.com>
2017-08-12 18:21:43 -0400

    Add newMessage and createMessage events.

---
+++ public/js/index.js
@@ -0,0 +1,20 @@
+
+let socket = io(); //"socket" is the websocket that is created by io();
+
+socket.on("connect", function () { //upon connecting to the server, callback below is called
+    console.log("Connected to server.");
+
+    socket.emit("createMessage", { //send info out
+        from: "chris",
+        text: "lolol"
+    })
+});
+
+socket.on("disconnect", function () { //calls callback function below when event occurs
+    console.log("Disconnected from server.");
+});
+
+socket.on("newMessage", function (message) {
+    console.log("New Message!", message);
+});
+
--- server/server.js
+++ server/server.js
@@ -1,12 +1,35 @@
 const path = require("path");
+const http = require("http"); //built in node module
 const express = require("express");
+const socketIO = require("socket.io");
 
 const publicPath = path.join(__dirname, "../public"); //this is a better way of doing paths when you need ot go up and out of folders
-const port= process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 let app = express();
+let server = http.createServer(app);  //this is already used in the background with express --. so can just use the express app as the argument
+let io = socketIO(server);  //"server" is the sever we want to use with our websockets. "io" is the websocket server
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on("connection", (socket) => { //.on registers an event listener. "connection" in this case --> lets you do something (in the callback function) when a user connects
+    //"socket" is the indiv connected, not all the users
+    console.log("New user connected.");
+
+    socket.emit("newMessage", {
+        to: "tara",
+        text: "hiya",
+        createdAt: 123
+    });
+
+    socket.on("createMessage",(message)=>{
+        console.log("Message received from client", message);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected.");
+    });
+});
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
